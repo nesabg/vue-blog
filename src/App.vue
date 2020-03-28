ff8e2ef18fdf055ed68d6e12e52db387a0a730c4
nesabg <nesabg2@gmail.com>
2020-03-28 23:42:38 +0200

    Adding basic some components and register form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-register></app-register>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Register from "./auth/Register.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
+  components: {
+    AppRegister: Register
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
 <style>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 1280px;
+  margin: 0 auto;
+  font-family: 'Courier New', Courier, monospace;
 }
+
+
 </style>
---
+++ src/auth/Register.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <h1>Register your free account</h1>
+    <form @submit.prevent="registerFormHandler">
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input id="name" type="text" v-model="name" />
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input id="email" type="email" v-model="email" />
+      </div>
+      <div class="form-group">
+        <label for="imgUrl">Image url:</label>
+        <input id="imgUrl" type="url" v-model="imgUrl" />
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input id="password" type="url" v-model="password" />
+      </div>
+      <div class="form-group">
+        <label for="rePassword">Repeat - password:</label>
+        <input id="rePassword" type="url" v-model="rePassword" />
+      </div>
+      <button>Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      imgUrl: "",
+      password: "",
+      rePassword: ""
+    };
+  },
+  methods: {
+      registerFormHandler() {
+          console.log('Form send')
+      }
+  }
+};
+</script>
+
+<style scoped>
+input {
+  width: 200px;
+  padding: 7px 10px;
+  margin: 10px;
+  display: inline-block;
+  border: 1px solid green;
+  border-radius: 10px;
+}
+
+label {
+  width: 100px;
+  padding: 7px 10px;
+  display: inline-block;
+  background-color: green;
+  border-radius: 10px;
+  font-size: 16px;
+  color: white;
+  cursor: pointer;
+}
+button {
+  padding: 7px 40px;
+  display: inline-block;
+  background-color: transparent;
+  border: 2px solid green;
+  border-radius: 10px;
+  color: green;
+  font-size: 16px;
+  cursor: pointer;
+
+}
+</style>
