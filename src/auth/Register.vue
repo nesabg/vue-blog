03804c5bcaff42fc334754fab762833dfc7939ea
nesabg <nesabg2@gmail.com>
2020-03-29 11:50:26 +0300

    Register form validation

--- src/auth/Register.vue
+++ src/auth/Register.vue
@@ -4,31 +4,55 @@
     <form @submit.prevent="registerFormHandler">
       <div class="form-group">
         <label for="name">Name:</label>
-        <input id="name" type="text" v-model="name" />
+        <input id="name" type="text" v-model="name" @blur="$v.name.$touch" />
+        <template v-if="$v.name.$error">
+            <span class="alert" v-if="!$v.name.required">The name is required</span>
+            <span class="alert" v-else-if="!$v.name.minLength">The name must be min 3 chars</span>
+        </template>
       </div>
       <div class="form-group">
         <label for="email">Email:</label>
-        <input id="email" type="email" v-model="email" />
+        <input id="email" type="email" v-model="email" @blur="$v.email.$touch"/>
+        <template v-if="$v.email.$error">
+            <span class="alert" v-if="!$v.email.required">The email is required</span>
+            <span class="alert" v-else-if="!$v.email.email">Not valid email</span>
+        </template>
       </div>
       <div class="form-group">
         <label for="imgUrl">Image url:</label>
-        <input id="imgUrl" type="url" v-model="imgUrl" />
+        <input id="imgUrl" type="url" v-model="imgUrl" @blur="$v.imgUrl.$touch" />
+        <template v-if="$v.imgUrl.$error">
+            <span class="alert" v-if="!$v.imgUrl.required">The image url is required</span>
+            <span class="alert" v-else-if="!$v.imgUrl.url">Not valid url</span>
+        </template>
       </div>
       <div class="form-group">
         <label for="password">Password:</label>
-        <input id="password" type="url" v-model="password" />
+        <input id="password" type="password" v-model="password" @blur="$v.password.$touch"/>
+        <template v-if="$v.password.$error">
+            <span class="alert" v-if="!$v.password.required">The password is required</span>
+            <span class="alert" v-else-if="!$v.password.minLength">Password must be minimum 8 symbols</span>
+            <span class="alert" v-else-if="!$v.password.maxLength">Password must be maximum 16 symbols</span>
+        </template>
       </div>
       <div class="form-group">
         <label for="rePassword">Repeat - password:</label>
-        <input id="rePassword" type="url" v-model="rePassword" />
+        <input id="rePassword" type="password" v-model.trim="$v.rePassword.$model" />
+        <template v-if="$v.rePassword.$error">
+            <span class="alert" v-if="!$v.rePassword.sameAsPass">Passwords is not the same</span>
+        </template>
       </div>
-      <button>Submit</button>
+      <button :disabled="$v.$invalid">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate';
+import { required, minLength, maxLength, email, url, sameAs } from 'vuelidate/lib/validators';
+//import { helpers } from 'vuelidate/lib/validators';
 export default {
+mixins: [validationMixin],
   data() {
     return {
       name: "",
@@ -41,6 +65,28 @@ export default {
   methods: {
       registerFormHandler() {
           console.log('Form send')
+      }
+  },
+  validations: {
+      name: {
+          required,
+          minLength: minLength(3)
+      },
+      email: {
+          required,
+          email
+      },
+      imgUrl: {
+          required,
+          url
+      },
+      password: {
+          required,
+          minLength: minLength(8),
+          maxLength: maxLength(16)
+      },
+      rePassword: {
+          sameAsPass: sameAs('password')
       }
   }
 };
@@ -75,6 +121,13 @@ button {
   color: green;
   font-size: 16px;
   cursor: pointer;
-
+}
+button:disabled {
+  border: 2px solid grey;
+  color: grey;
+  cursor:progress;
+}
+.alert {
+    color: red;
 }
 </style>
